77cc99082b6d4c4eb7f6a52b460fbccd4c1d8011
tomotaka yonaha <tomosuuu71@gmail.com>
2020-01-24 11:38:23 +0900

    kinamatic_dataset適用実験

--- SupportVectorRegression.py
+++ SupportVectorRegression.py
@@ -4,19 +4,17 @@ from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 import scipy.stats
-
-data = []
-target = []
-
-for i in range(1000):
-    data.append([i,i,i])
-    target.append(i)
+import pandas as pd
 
 
-data = np.array(data)
-target = np.array(target)
+d = pd.read_csv("kinematic_dataset.csv", index_col=0)
+data = d.values
 
-x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2)
+t = pd.read_csv("Evaluate_Flare/evaluate-evaluate.csv", index_col=0)
+target = t.T.values[0]
+
+
+x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.1)
 
 # x_train = scipy.stats.zscore(x_train)
 # x_test = scipy.stats.zscore(x_test)
@@ -24,8 +22,8 @@ x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2)
 reg = svm.SVR(kernel='rbf', C=1e3, gamma=0.1).fit(x_train, y_train)
 rbf_reg = reg.predict(x_test)
 
-print(x_train)
-print(x_test)
+print("x_train:",x_train)
+print("x_test:",x_test)
 print(y_train)
 
 print("----")
@@ -42,11 +40,11 @@ plt.title("Nikkei Stock Average")
 plt.xlabel('day')
 plt.ylabel('stock price')
 plt.plot(xjiku,rbf_reg,linewidth=4,color="orange",label='predict value')
-#plt.scatter(xjiku, y_test,label='test_data')
+plt.scatter(xjiku, y_test,label='test_data')
 plt.plot(xjiku, y_test,label='test_data')
 plt.legend()
 plt.show()
 
-score = cross_val_score()
+# score = cross_val_score()
 
 
