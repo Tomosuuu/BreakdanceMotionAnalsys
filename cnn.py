24af9b035ad717c2be0f2d35552546a7457e2b8f
tomotaka yonaha <tomosuuu71@gmail.com>
2020-01-31 15:53:06 +0900

    cnn適用実験

--- cnn.py
+++ cnn.py
@@ -1,7 +1,18 @@
 import keras
+import pandas as pd
+import numpy as np
 from keras.datasets import boston_housing
+from sklearn.model_selection import train_test_split
 
-(X_train, y_train), (X_test, y_test) = boston_housing.load_data()
+d = pd.read_csv("kinematic_dataset.csv", index_col=0)
+data = d.values
+
+t = pd.read_csv("Evaluate_Flare/evaluate-evaluate.csv", index_col=0)
+target = t.T.values[0]
+
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1)
+
+# (X_train, y_train), (X_test, y_test) = boston_housing.load_data()
 
 # トレーニングデータの正規化
 X_train_mean = X_train.mean(axis=0)
@@ -9,12 +20,14 @@ X_train_std = X_train.std(axis=0)
 X_train -= X_train_mean
 X_train /= X_train_std
 
+y_train = np.array(y_train, dtype=np.float)
 y_train_mean = y_train.mean()
 y_train_std = y_train.std()
 y_train -= y_train_mean
 y_train /= y_train_std
 
 # テストデータの正規化
+y_test = np.array(y_test, dtype=np.float)
 X_test -= X_train_mean
 X_test /= X_train_std
 y_test -= y_train_mean
@@ -39,4 +52,5 @@ history = model.fit(X_train, y_train,
                     validation_data=(X_test, y_test))
 
 expect = model.predict(X_test, batch_size=5)
+print(y_test)
 print(expect)
