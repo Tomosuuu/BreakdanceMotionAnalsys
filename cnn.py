3c60d57ac31c8986a2f93386c3fee78d4750b890
tomotaka yonaha <tomosuuu71@gmail.com>
2020-02-07 21:25:27 +0900

    change parameter

--- cnn.py
+++ cnn.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 d = pd.read_csv("kinematic_dataset.csv", index_col=0)
 data = d.values
@@ -10,34 +11,18 @@ data = d.values
 t = pd.read_csv("Evaluate_Flare/evaluate-evaluate.csv", index_col=0)
 target = t.T.values[0]
 
-X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.1)
+X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=3)
 
-# トレーニングデータの正規化
-X_train_mean = X_train.mean(axis=0)
-X_train_std = X_train.std(axis=0)
-X_train -= X_train_mean
-X_train /= X_train_std
-#
-# y_train = np.array(y_train, dtype=np.float)
-# y_train_mean = y_train.mean()
-# y_train_std = y_train.std()
-# y_train -= y_train_mean
-# y_train /= y_train_std
-#
-# # テストデータの正規化
-# y_test = np.array(y_test, dtype=np.float)
-X_test -= X_train_mean
-X_test /= X_train_std
-# y_test -= y_train_mean
-# y_test /= y_train_std
-
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
 
 model = keras.Sequential([
-    keras.layers.Dense(216, activation='relu', input_shape=(204,)),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(216, activation='relu'),
-    keras.layers.Dropout(0.5),
-    keras.layers.Dense(50, activation='relu'),
+    keras.layers.Dense(216, activation='relu', input_shape=(425,)),
+    keras.layers.Dropout(0.3),
+    keras.layers.Dense(10, activation='relu'),
+    # keras.layers.Dropout(0.5),
+    # keras.layers.Dense(40, activation='relu'),
     # keras.layers.Dense(600, activation='relu'),
     # keras.layers.Dense(300, activation='relu'),
     keras.layers.Dense(6, activation='softmax')
@@ -50,7 +35,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 history = model.fit(X_train, y_train,
-                    epochs=300,
+                    epochs=50,
                     batch_size=32,
                     validation_data=(X_test, y_test))
 
@@ -61,7 +46,7 @@ print('\nTest accuracy:', test_acc)
 expect = model.predict(X_test)
 print(y_test)
 # print(expect)
-print(np.argmax(expect[0]),np.argmax(expect[1]),np.argmax(expect[2]),np.argmax(expect[3]),np.argmax(expect[4]),np.argmax(expect[5]),np.argmax(expect[6]))
+print(np.argmax(expect[0]),np.argmax(expect[1]),np.argmax(expect[2]))
 
 # Plot training & validation accuracy values
 plt.plot(history.history['accuracy'])
@@ -70,6 +55,7 @@ plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig("./Graph/accuracy.png")
 plt.show()
 
 # Plot training & validation loss values
@@ -79,4 +65,5 @@ plt.title('Model loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
+plt.savefig("./Graph/loss.png")
 plt.show()
