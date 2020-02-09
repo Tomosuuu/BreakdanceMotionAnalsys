4d7f5982fb08fa62e3da86971533919b41ba768c
tomotaka yonaha <tomosuuu71@gmail.com>
2020-02-09 18:19:10 +0900

    show graph

--- cnn.py
+++ cnn.py
@@ -4,66 +4,94 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_squared_error
 
-d = pd.read_csv("kinematic_dataset.csv", index_col=0)
-data = d.values
+plt.rcParams["font.size"] = 30
 
-t = pd.read_csv("Evaluate_Flare/evaluate-evaluate.csv", index_col=0)
-target = t.T.values[0]
+class CNN():
+    def __init__(self):
+        err_list = []
+        for i in range(10):
+            test, result = self.cnn(i)
+            err = np.sqrt(mean_squared_error(test, result))
+            err_list.append(err)
 
-X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=3)
+        # print(err_list)
+        plt.figure(figsize=(20, 10), dpi=200)
+        plt.hist(err_list)
+        plt.savefig("./Graph/rmse.png")
 
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.fit_transform(X_test)
+    def cnn(self, num):
+        d = pd.read_csv("kinematic_dataset.csv", index_col=0)
+        data = d.values
 
-model = keras.Sequential([
-    keras.layers.Dense(216, activation='relu', input_shape=(425,)),
-    keras.layers.Dropout(0.3),
-    keras.layers.Dense(10, activation='relu'),
-    # keras.layers.Dropout(0.5),
-    # keras.layers.Dense(40, activation='relu'),
-    # keras.layers.Dense(600, activation='relu'),
-    # keras.layers.Dense(300, activation='relu'),
-    keras.layers.Dense(6, activation='softmax')
-])
+        t = pd.read_csv("Evaluate_Flare/evaluate-evaluate.csv", index_col=0)
+        target = t.T.values[0]
 
-model.summary()
+        X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=3)
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.fit_transform(X_test)
 
-history = model.fit(X_train, y_train,
-                    epochs=50,
-                    batch_size=32,
-                    validation_data=(X_test, y_test))
+        model = keras.Sequential([
+            keras.layers.Dense(216, activation='relu', input_shape=(425,)),
+            keras.layers.Dropout(0.3),
+            keras.layers.Dense(10, activation='relu'),
+            # keras.layers.Dropout(0.5),
+            # keras.layers.Dense(40, activation='relu'),
+            # keras.layers.Dense(600, activation='relu'),
+            # keras.layers.Dense(300, activation='relu'),
+            keras.layers.Dense(6, activation='softmax')
+        ])
 
-test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+        model.summary()
 
-print('\nTest accuracy:', test_acc)
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
 
-expect = model.predict(X_test)
-print(y_test)
-# print(expect)
-print(np.argmax(expect[0]),np.argmax(expect[1]),np.argmax(expect[2]))
+        history = model.fit(X_train, y_train,
+                            epochs=50,
+                            batch_size=32,
+                            validation_data=(X_test, y_test))
 
-# Plot training & validation accuracy values
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig("./Graph/accuracy.png")
-plt.show()
+        test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
 
-# Plot training & validation loss values
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Test'], loc='upper left')
-plt.savefig("./Graph/loss.png")
-plt.show()
+        print('\nTest accuracy:', test_acc)
+
+        expect = model.predict(X_test)
+        print(y_test)
+        # print(expect)
+        result = []
+        result.append(np.argmax(expect[0]))
+        result.append(np.argmax(expect[1]))
+        result.append(np.argmax(expect[2]))
+        print(result)
+
+        # Plot training & validation accuracy values
+        plt.figure(figsize=(20, 10), dpi=200)
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('Model accuracy')
+        plt.ylabel('Accuracy')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.savefig("./Graph/accuracy(" + str(num) + ").png")
+        # plt.show()
+
+        # Plot training & validation loss values
+        plt.figure(figsize=(20, 10), dpi=200)
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Epoch')
+        plt.legend(['Train', 'Test'], loc='upper left')
+        plt.savefig("./Graph/loss(" + str(num) + ").png")
+        # plt.show()
+
+        return y_test, result
+
+
+CNN()
